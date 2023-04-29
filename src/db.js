b665f97fd88c726b53c51e7d497e56c572384cfa
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-04-29 22:41:10 +0200

    Update data

--- src/db.js
+++ src/db.js
@@ -2,33 +2,33 @@ export const fields = [
   [
     {
       id: 1,
-      name: "Quiz App",
+      name: "Corner 1",
       fieldNumber: 4,
       message: "Challenge Time! Can you apply your current learnings?",
     },
     {
       id: 2,
-      name: "JavaScript Forms",
+      name: "Field",
       fieldNumber: 5,
       message:
         "This one is a tough topic. Try to gain the chance to free yourself from here by rolling the dice.",
     },
     {
       id: 3,
-      name: "Async JavaScript",
+      name: "Field",
       fieldNumber: 6,
       message:
         "Yeah! Let's learn more about it to fetch what we can. We gonna fetch them all!",
     },
     {
       id: 4,
-      name: "JavaScript Structure",
+      name: "Field",
       fieldNumber: 7,
       message: "If we have no structure we do not find anything in our code.",
     },
     {
       id: 5,
-      name: "Recap",
+      name: "Corner 2",
       fieldNumber: 8,
       message:
         "You arrived on the recap field for endlessly repeating the learned topics by coding some recap projects. Try to show that you can apply your knowledge and that you are ready to move forward.",
@@ -37,20 +37,20 @@ export const fields = [
   [
     {
       id: 3,
-      name: "JavaScript Basics",
+      name: "Field",
       fieldNumber: 3,
       message:
         "We have structure, we have stylings, now we want interaction! Let's go!",
     },
     {
       id: 2,
-      name: "CSS",
+      name: "Field",
       fieldNumber: 2,
       message: "Styling seems more complicated than it seems.",
     },
     {
       id: 1,
-      name: "HTML",
+      name: "Field",
       fieldNumber: 1,
       message: "Nice start! Let's learn all about semantics.",
     },
@@ -58,14 +58,14 @@ export const fields = [
   [
     {
       id: 1,
-      name: "React Props",
+      name: "Field",
       fieldNumber: 9,
       message: "Challenge accepted! Understand the basics.",
     },
-    { id: 2, name: "React State", fieldNumber: 10, message: "..." },
+    { id: 2, name: "Field", fieldNumber: 10, message: "..." },
     {
       id: 3,
-      name: "Styled Components",
+      name: "Field",
       fieldNumber: 11,
       message:
         "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
@@ -81,23 +81,23 @@ export const fields = [
     },
     {
       id: 2,
-      name: "Backend",
+      name: "Field",
       fieldNumber: 15,
       message:
         "Optional or not. This step also needs to be learned. Let us stay here before we start with our capstone project.",
     },
     {
       id: 3,
-      name: "Next.js",
+      name: "Field",
       fieldNumber: 14,
       message: "Next level! Now we have learned the fundamentals of react.",
     },
     {
       id: 4,
-      name: "Component Testing",
+      name: "Field",
       fieldNumber: 13,
       message: "Nice start!",
     },
-    { id: 5, name: "Bergfest", fieldNumber: 12, message: "Nice start!" },
+    { id: 5, name: "Corner 3", fieldNumber: 12, message: "Nice start!" },
   ],
 ];
