e8abf3d0913f0b197d29352ebad6b8a4fc8bc092
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-04-29 10:53:11 +0200

    Create panel component

---
+++ src/components/Panel.tsx
@@ -0,0 +1,46 @@
+import styled, { css } from "styled-components";
+import FieldItem from "./FieldItem";
+import type { Field } from "../types/Field";
+
+interface PanelProps {
+  fields: Field[];
+  variant: string;
+  playerLocation: number;
+}
+
+export default function Panel({ fields, variant, playerLocation }: PanelProps) {
+  return (
+    <List variant={variant}>
+      {fields.map((field) => (
+        <FieldItem
+          key={field.id}
+          name={field.name}
+          fieldNumber={field.fieldNumber}
+          playerLocation={playerLocation}
+        />
+      ))}
+    </List>
+  );
+}
+
+const List = styled.ul.attrs({
+  role: "list",
+})<{ variant: string }>`
+  display: grid;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  word-break: break-word;
+  ${({ variant }) =>
+    variant === "vertical" &&
+    css`
+      width: 8rem;
+      grid-template-rows: 1fr 1fr 1fr;
+    `}
+  ${({ variant }) =>
+    variant === "horizontal" &&
+    css`
+      height: 8rem;
+      grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    `}
+`;
