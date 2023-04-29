74d68b48ce328d0f61ca346f51d7795d9135df57
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-04-29 10:52:24 +0200

    Add example data

---
+++ src/db.js
@@ -0,0 +1,103 @@
+export const fields = [
+  [
+    {
+      id: 1,
+      name: "Quiz App",
+      fieldNumber: 4,
+      message: "Challenge Time! Can you apply your current learnings?",
+    },
+    {
+      id: 2,
+      name: "JavaScript Forms",
+      fieldNumber: 5,
+      message:
+        "This one is a tough topic. Try to gain the chance to free yourself from here by rolling the dice.",
+    },
+    {
+      id: 3,
+      name: "Async JavaScript",
+      fieldNumber: 6,
+      message:
+        "Yeah! Let's learn more about it to fetch what we can. We gonna fetch them all!",
+    },
+    {
+      id: 4,
+      name: "JavaScript Structure",
+      fieldNumber: 7,
+      message: "If we have no structure we do not find anything in our code.",
+    },
+    {
+      id: 5,
+      name: "Recap",
+      fieldNumber: 8,
+      message:
+        "You arrived on the recap field for endlessly repeating the learned topics by coding some recap projects. Try to show that you can apply your knowledge and that you are ready to move forward.",
+    },
+  ],
+  [
+    {
+      id: 3,
+      name: "JavaScript Basics",
+      fieldNumber: 3,
+      message:
+        "We have structure, we have stylings, now we want interaction! Let's go!",
+    },
+    {
+      id: 2,
+      name: "CSS",
+      fieldNumber: 2,
+      message: "Styling seems more complicated than it seems.",
+    },
+    {
+      id: 1,
+      name: "HTML",
+      fieldNumber: 1,
+      message: "Nice start! Let's learn all about semantics.",
+    },
+  ],
+  [
+    {
+      id: 1,
+      name: "React Props",
+      fieldNumber: 9,
+      message: "Challenge accepted! Understand the basics.",
+    },
+    { id: 2, name: "React State", fieldNumber: 10, message: "..." },
+    {
+      id: 3,
+      name: "Styled Components",
+      fieldNumber: 11,
+      message:
+        "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
+    },
+  ],
+  [
+    {
+      id: 1,
+      name: "Start",
+      fieldNumber: 0,
+      message:
+        "Welcome to our tour to learn to code! Let's see how the journey will develop.",
+    },
+    {
+      id: 2,
+      name: "Backend",
+      fieldNumber: 15,
+      message:
+        "Optional or not. This step also needs to be learned. Let us stay here before we start with our capstone project.",
+    },
+    {
+      id: 3,
+      name: "Next.js",
+      fieldNumber: 14,
+      message: "Next level! Now we have learned the fundamentals of react.",
+    },
+    {
+      id: 4,
+      name: "Component Testing",
+      fieldNumber: 13,
+      message: "Nice start!",
+    },
+    { id: 5, name: "Bergfest", fieldNumber: 12, message: "Nice start!" },
+  ],
+];
