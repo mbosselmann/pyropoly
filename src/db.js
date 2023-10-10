4f05dbd4a75c4d5cbac9f3830b0bf04c7515c45a
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-10-10 21:25:17 +0200

    Update data

--- src/db.js
+++ src/db.js
@@ -2,69 +2,80 @@ export const fields = [
   [
     {
       id: 1,
-      name: "Corner 1",
+      name: "Legacy Code Labyrinth",
+      type: "jail",
       fieldNumber: 4,
       message: "Challenge Time! Can you apply your current learnings?",
     },
     {
       id: 2,
-      name: "Field",
+      name: "Var City",
+      type: "property",
       fieldNumber: 5,
       message:
         "This one is a tough topic. Try to gain the chance to free yourself from here by rolling the dice.",
     },
     {
       id: 3,
-      name: "Field",
+      name: "Lockfile Lounge",
+      type: "utility",
       fieldNumber: 6,
       message:
         "Yeah! Let's learn more about it to fetch what we can. We gonna fetch them all!",
     },
     {
       id: 4,
-      name: "Field",
+      name: "Function Falls",
+      type: "property",
       fieldNumber: 7,
       message: "If we have no structure we do not find anything in our code.",
     },
     {
       id: 5,
-      name: "Field",
+      name: "Array Arcade",
+      type: "property",
       fieldNumber: 8,
       message: "If we have no structure we do not find anything in our code.",
     },
     {
       id: 6,
-      name: "Field",
+      name: "Context Crossing",
+      type: "railroad",
       fieldNumber: 9,
       message: "If we have no structure we do not find anything in our code.",
     },
     {
       id: 7,
-      name: "Field",
+      name: "DOMinion Domain",
+      type: "property",
       fieldNumber: 10,
       message: "If we have no structure we do not find anything in our code.",
     },
     {
       id: 8,
-      name: "Field",
+      name: "Refactor Rewards",
+      type: "chest",
       fieldNumber: 11,
       message: "If we have no structure we do not find anything in our code.",
     },
     {
       id: 9,
-      name: "Field",
+      name: "Await Alley",
+      type: "property",
       fieldNumber: 12,
       message: "If we have no structure we do not find anything in our code.",
     },
     {
       id: 10,
-      name: "Field",
+      name: "Promise Park",
+      type: "property",
       fieldNumber: 13,
       message: "If we have no structure we do not find anything in our code.",
     },
     {
       id: 11,
-      name: "Corner 2",
+      name: "Open Source Oasis",
+      type: "bonus",
       fieldNumber: 14,
       message:
         "You arrived on the recap field for endlessly repeating the learned topics by coding some recap projects. Try to show that you can apply your knowledge and that you are ready to move forward.",
@@ -73,62 +84,71 @@ export const fields = [
   [
     {
       id: 9,
-      name: "Field",
+      name: "Grid Gardens",
+      type: "property",
       fieldNumber: 9,
       message:
         "We have structure, we have stylings, now we want interaction! Let's go!",
     },
     {
       id: 8,
-      name: "Field",
+      name: "Form Fields Farm",
+      type: "property",
       fieldNumber: 8,
       message:
         "We have structure, we have stylings, now we want interaction! Let's go!",
     },
     {
       id: 7,
-      name: "Field",
+      name: "Debugging Dilemmas",
+      type: "chance",
       fieldNumber: 7,
       message:
         "We have structure, we have stylings, now we want interaction! Let's go!",
     },
     {
       id: 6,
-      name: "Field",
+      name: "Flexbox Flats",
+      type: "property",
       fieldNumber: 6,
       message:
         "We have structure, we have stylings, now we want interaction! Let's go!",
     },
     {
       id: 5,
-      name: "Field",
+      name: "JSX Junction",
+      type: "railroad",
       fieldNumber: 5,
       message:
         "We have structure, we have stylings, now we want interaction! Let's go!",
     },
     {
       id: 4,
-      name: "Field",
+      name: "Dependency Debt",
+      type: "tax",
       fieldNumber: 4,
       message:
         "We have structure, we have stylings, now we want interaction! Let's go!",
     },
     {
       id: 3,
-      name: "Field",
+      name: "Selectorville",
+      type: "property",
       fieldNumber: 3,
       message:
         "We have structure, we have stylings, now we want interaction! Let's go!",
     },
     {
       id: 2,
-      name: "Field",
+      name: "Bug & Fix",
+      type: "chest",
       fieldNumber: 2,
       message: "Styling seems more complicated than it seems.",
     },
     {
       id: 1,
-      name: "Field",
+      name: "Pixel Playground",
+      type: "property",
       fieldNumber: 1,
       message: "Nice start! Let's learn all about semantics.",
     },
@@ -136,56 +156,70 @@ export const fields = [
   [
     {
       id: 1,
-      name: "Field",
+      name: "ES6 Estates",
+      type: "property",
       fieldNumber: 9,
       message: "Challenge accepted! Understand the basics.",
     },
-    { id: 2, name: "Field", fieldNumber: 10, message: "..." },
     {
-      id: 3,
-      name: "Field",
-      fieldNumber: 11,
-      message:
-        "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
+      id: 2,
+      name: "Pull Possibilities",
+      type: "chance",
+      fieldNumber: 10,
+      message: "...",
     },
     {
-      id: 4,
-      name: "Field",
+      id: 3,
+      name: "API Avenue",
+      type: "property",
       fieldNumber: 12,
       message:
         "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
     },
     {
-      id: 5,
-      name: "Field",
+      id: 4,
+      name: "Fetch Frontier",
+      type: "property",
       fieldNumber: 13,
       message:
         "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
     },
     {
+      id: 5,
+      name: "Router Rails",
+      type: "railroad",
+      fieldNumber: 11,
+      message:
+        "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
+    },
+    {
       id: 6,
-      name: "Field",
+      name: "Component Castle",
+      type: "property",
       fieldNumber: 14,
       message:
         "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
     },
     {
       id: 7,
-      name: "Field",
+      name: "Hook Haven",
+      type: "property",
       fieldNumber: 15,
       message:
         "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
     },
     {
       id: 8,
-      name: "Field",
+      name: "Package Power Plant",
+      type: "utility",
       fieldNumber: 16,
       message:
         "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
     },
     {
       id: 9,
-      name: "Field",
+      name: "Route Royale",
+      type: "property",
       fieldNumber: 17,
       message:
         "Nice to be here and relax. Throw the dice to see if you want to stay here longer.",
@@ -194,68 +228,84 @@ export const fields = [
   [
     {
       id: 1,
-      name: "Start",
+      name: "Go: Launchpad!",
+      type: "start",
       fieldNumber: 0,
       message:
         "Welcome to our tour to learn to code! Let's see how the journey will develop.",
     },
     {
       id: 2,
-      name: "Field",
+      name: "Reducer Residence",
+      type: "property",
+
       fieldNumber: 15,
       message:
         "Optional or not. This step also needs to be learned. Let us stay here before we start with our capstone project.",
     },
     {
       id: 3,
-      name: "Field",
+      name: "Callback Hell Toll",
+      type: "tax",
       fieldNumber: 14,
       message: "Next level! Now we have learned the fundamentals of react.",
     },
     {
       id: 4,
-      name: "Field",
+      name: "Lifecycle Loft",
+      type: "property",
       fieldNumber: 13,
       message: "Nice start!",
     },
     {
       id: 5,
-      name: "Field",
+      name: "Beta Box",
+      type: "chance",
       fieldNumber: 14,
       message: "Nice start!",
     },
     {
       id: 6,
-      name: "Field",
+      name: "State Station",
+      type: "railroad",
       fieldNumber: 15,
       message: "Nice start!",
     },
     {
       id: 7,
-      name: "Field",
+      name: "Middleware Meadow",
+      type: "property",
       fieldNumber: 16,
       message: "Nice start!",
     },
     {
       id: 8,
-      name: "Field",
+      name: "Patch Packages",
+      type: "chest",
       fieldNumber: 17,
       message: "Nice start!",
     },
     {
       id: 9,
-      name: "Field",
+      name: "Expressway Estates",
       fieldNumber: 18,
+      type: "property",
       message: "Nice start!",
     },
     {
       id: 10,
-      name: "Field",
+      name: "Node Nook",
       fieldNumber: 19,
+      type: "property",
       message: "Nice start!",
     },
-
-    { id: 11, name: "Corner 3", fieldNumber: 20, message: "Nice start!" },
+    {
+      id: 11,
+      name: "Go to Legacy Code Labyrinth",
+      type: "jail",
+      fieldNumber: 20,
+      message: "Nice start!",
+    },
   ],
 ];
 
