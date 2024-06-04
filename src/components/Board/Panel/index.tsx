2a5f44fe4458c6e1c7fb064cf76c300b7092d08f
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2024-06-04 21:36:29 +0200

    update panel component

--- src/components/Board/Panel/index.tsx
+++ src/components/Board/Panel/index.tsx
@@ -1,30 +1,6 @@
-import styled, { css } from "styled-components";
+import styles from "./styles.module.css";
 import FieldItem from "./FieldItem";
 import type { Field } from "../../../types/Field";
-
-const List = styled.ul.attrs({
-  role: "list",
-})<{ variant: string }>`
-  display: grid;
-  padding: 0;
-  margin: 0;
-  list-style: none;
-  word-break: break-word;
-  font-size: 0.5rem;
-  ${({ variant }) =>
-    (variant === "vertical-left" || variant === "vertical-right") &&
-    css`
-      width: var(--field-size);
-      grid-template-rows: repeat(9, 1fr);
-    `}
-  ${({ variant }) =>
-    (variant === "horizontal-top" || variant === "horizontal-bottom") &&
-    css`
-      height: var(--field-size);
-      grid-template-columns: var(--field-size) repeat(9, 1fr) var(--field-size);
-    `}
-`;
-
 interface PanelProps {
   fields: Field[];
   variant: string;
@@ -32,7 +8,7 @@ interface PanelProps {
 
 export default function Panel({ fields, variant }: PanelProps) {
   return (
-    <List variant={variant}>
+    <ul role="list" className={`${styles.list} ${styles[variant]}`}>
       {fields.map((field) => (
         <FieldItem
           key={field.id}
@@ -44,6 +20,6 @@ export default function Panel({ fields, variant }: PanelProps) {
           price={field.price}
         />
       ))}
-    </List>
+    </ul>
   );
 }
