8041305953500cfc8e10ac3588d01198ad79c2a6
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2024-06-05 22:09:40 +0200

    move global styles files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
-import "../../styles.css";
-import "../../themes.styles.css";
+import "../styles.css";
+import "../themes.styles.css";
 import { PlayersProvider } from "@/context";
 
 export default function App({ Component, pageProps }: AppProps) {
